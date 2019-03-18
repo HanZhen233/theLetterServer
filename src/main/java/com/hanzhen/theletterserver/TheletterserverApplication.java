a8ed0d4c7201ca70a2662f675fb0f256fcdc25a6
HanZhen223 <1005256626@qq.com>
2019-03-18 18:27:47 +0800

    初始化项目

---
+++ src/main/java/com/hanzhen/theletterserver/TheletterserverApplication.java
@@ -0,0 +1,13 @@
+package com.hanzhen.theletterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheletterserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheletterserverApplication.class, args);
+	}
+
+}
